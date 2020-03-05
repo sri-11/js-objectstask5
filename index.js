1205d10da317a1e3aa2128e57f78e025630072b2
sri-11 <bksubiksha@gmail.com>
2020-03-05 11:53:00 +0530

    1 file modified

--- index.js
+++ index.js
@@ -50,28 +50,37 @@ console.log("Rollno:",Rollno);
 ob.setRollno(15);
 Rollno=ob.getRollno();
 console.log("Rollno:",Rollno);
+
 let Name=ob.getName();
 console.log("Name:",Name);
 ob.setName("Srisubiksha Balachandar");
 Name=ob.getName();
 console.log("Name:",Name);
+
 let Dept=ob.getDept();
 console.log("Dept:",Dept);
 ob.setDept("Mech");
 Dept=ob.getDept();
 console.log("Dept:",Dept);
+
 let Type=ob.getType();
 console.log("Type:",Type);
 ob.setType("Very Good Girl");
 Type=ob.getType();
 console.log("Type:",Type);
+
 let College=ob.getCollege();
 console.log("College:",College);
 ob.setCollege("Sri Krishna College of Technology");
 College=ob.getCollege();
 console.log("College:",College);
+
 let Interest=ob.getInterest();
 console.log("Interest:",Interest);
 ob.setInterest("Sidney sheldon novels");
 Interest=ob.getInterest();
 console.log("Interest:",Interest);
+
+
+//-----------------------------------------------------//
+
